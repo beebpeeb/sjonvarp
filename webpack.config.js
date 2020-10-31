93a776a1d83bf2e73661d8d8d7a67378a4783652
Paul Burt <peeb@protonmail.com>
2020-10-31 09:49:36 +0000

    Updated Bulma CSS. Tweaked Webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: path.resolve(__dirname, "target", "public", "js", "index.js"),
   output: {
-    path: path.join(__dirname, "resources", "public", "js"),
+    path: path.join(__dirname, "target", "public", "js"),
     filename: "main.js"
   }
 };
