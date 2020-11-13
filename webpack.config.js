449f32a05c5729d227a7e5f60a6fd9c587d80103
Paul Burt <peeb@protonmail.com>
2020-11-13 11:19:57 +0000

    Simplified build. Removed Webpack config.

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: path.resolve(__dirname, "target", "public", "js", "index.js"),
-  output: {
-    path: path.join(__dirname, "resources", "public", "js"),
-    filename: "main.js"
-  }
-};
